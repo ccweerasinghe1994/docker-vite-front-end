983c2b46989a661b4b8afc3ec02e4fdb09c094c2
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-12-15 16:58:30 +0530

    adding small tests

---
+++ src/App.test.js
@@ -0,0 +1,4 @@
+it('it should summarize all number values in an Array', () => {
+  const result = 6;
+  expect(result).toBe(6);
+});
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    // ...
     globals: true,
   },
-
   server: {
     host: true,
     port: 5173,

891b7820f21cefb745a49f5305fb172f81e34ed4
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-12-15 15:13:29 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+  },
+
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
